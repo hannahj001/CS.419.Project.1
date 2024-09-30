9bb3d34e4ae27a17ad653ab63e3ccdea1532b4b1
hannahj001 <hannahjohnstongk@gmail.com>
2024-09-30 17:03:21 -0400

    SJF finished

--- mainFiles/src/SJF.java
+++ mainFiles/src/SJF.java
@@ -1,40 +1,59 @@
 package mainFiles.src;
 //THIS FILE CAN BE CHANGED
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Shortest Job First.
  */
 public class SJF implements Scheduler {
+	private List<SimProcess> queue;
 
+	public SJF() {
+		queue = new ArrayList<>();
+	}
 	@Override
 	public void onProcessArrival(SimProcess p, int time) {
-		// TODO Auto-generated method stub
+		queue.add(p);
+		System.out.println("Process {" + "Id=" + p.getId() + ", Arrival Time=" + p.getTimeOfArrival() + ", Burst Time=" + p.getBurstTime() + "} arrived at time " + time);
+		queue.sort(Comparator.comparingInt(SimProcess::getBurstTime));
+		if (queue.size() == 1) {
+			System.out.println("Start running Process {" + "Id=" + queue.get(0).getId() + ", Arrival Time=" + queue.get(0).getTimeOfArrival() + ", Burst Time=" + queue.get(0).getBurstTime() + ", Current Time=" + time + "}");
+		}
 	}
 
 	@Override
 	public void onProcessExit(SimProcess p, int time) {
-		// TODO Auto-generated method stub
+		queue.remove(p);
+		System.out.println("Process {" + "Id=" + p.getId() + "} finished at time " + time);
+		if (!queue.isEmpty()) {
+			SimProcess nextProcess = queue.get(0);
+			System.out.println("Start running Process {" + "Id=" + nextProcess.getId() + ", Arrival Time=" + nextProcess.getTimeOfArrival() + ", Burst Time=" + nextProcess.getBurstTime() + ", Current Time=" + time + "}");
+		}
 	}
 
 	@Override
 	public void onClockInterrupt(int timeElapsed, int time) {
-		// TODO Auto-generated method stub
+		//none
 	}
 
 	@Override
 	public String getAlgorithmName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "SJF";
 	}
 
 	@Override
 	public SimProcess currentProcess() {
-		// TODO Auto-generated method stub
-		return null;
+		if (queue.isEmpty()) {
+			return null;
+		}
+		return queue.get(0);
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return queue.isEmpty();
 	}
 }
