675738a0fc81b37574149d9a3eb7e5d813d5f124
hannahj001 <hannahjohnstongk@gmail.com>
2024-10-01 13:54:12 -0400

    Need to get SJF to start p2 first

--- mainFiles/src/SJF.java
+++ mainFiles/src/SJF.java
@@ -10,27 +10,48 @@ import java.util.List;
  */
 public class SJF implements Scheduler {
 	private List<SimProcess> queue;
+	private SimProcess currentProcess;
+	private List<Integer> waitingTimes;
+	private int totalWaitingTime = 0;
+
+
 
 	public SJF() {
 		queue = new ArrayList<>();
+		waitingTimes = new ArrayList<Integer>();
+		currentProcess = null;
 	}
 	@Override
 	public void onProcessArrival(SimProcess p, int time) {
+		//System.out.println("Arrival Process {" + "Id=" + p.getId() + ", Arrival Time=" + p.getTimeOfArrival() + ", Burst Time=" + p.getBurstTime() + ", Current Time=" + time + "}");
 		queue.add(p);
-		System.out.println("Process {" + "Id=" + p.getId() + ", Arrival Time=" + p.getTimeOfArrival() + ", Burst Time=" + p.getBurstTime() + "} arrived at time " + time);
-		queue.sort(Comparator.comparingInt(SimProcess::getBurstTime));
-		if (queue.size() == 1) {
-			System.out.println("Start running Process {" + "Id=" + queue.get(0).getId() + ", Arrival Time=" + queue.get(0).getTimeOfArrival() + ", Burst Time=" + queue.get(0).getBurstTime() + ", Current Time=" + time + "}");
+
+		queue.sort(Comparator.comparingInt(SimProcess::getTimeOfArrival));
+		if (currentProcess == null || (p.getTimeOfArrival() == currentProcess.getTimeOfArrival() && p.getBurstTime() <currentProcess.getBurstTime())) {
+			currentProcess = p;
+
+
+			//System.out.printf("\nis current process finished: %s \n",currentProcess().isFinished());
+		    //System.out.printf("\nis current process: %s \n",currentProcess().getId());
+			System.out.println("Start running Process {Id=" + currentProcess.getId() + ", Arrival Time=" + currentProcess.getTimeOfArrival() + ", Burst Time=" + currentProcess.getBurstTime() + ", Current Time=" + time + "}");
+
 		}
-	}
+		}
 
 	@Override
 	public void onProcessExit(SimProcess p, int time) {
 		queue.remove(p);
-		System.out.println("Process {" + "Id=" + p.getId() + "} finished at time " + time);
+		//System.out.println("Process {" + "Id=" + p.getId() + "} finished at time " + time);
+		int waitingTime = time - p.getTimeOfArrival() - p.getBurstTime();
+		waitingTimes.add(waitingTime);
+		totalWaitingTime += waitingTime;
+
+		System.out.println(p.getId() + " finished at time " + time + ". Its waiting time is " + waitingTime);
+		System.out.println("Current average waiting time: " + calculateAvgWaiting());
+
 		if (!queue.isEmpty()) {
-			SimProcess nextProcess = queue.get(0);
-			System.out.println("Start running Process {" + "Id=" + nextProcess.getId() + ", Arrival Time=" + nextProcess.getTimeOfArrival() + ", Burst Time=" + nextProcess.getBurstTime() + ", Current Time=" + time + "}");
+			queue.sort(Comparator.comparingInt(SimProcess::getBurstTime));
+			System.out.println("Start running Process {" + "Id=" + queue.getFirst().getId() + ", Arrival Time=" + queue.getFirst().getTimeOfArrival() + ", Burst Time=" + queue.getFirst().getBurstTime() + ", Current Time=" + time + "}");
 		}
 	}
 
@@ -38,6 +59,15 @@ public class SJF implements Scheduler {
 	public void onClockInterrupt(int timeElapsed, int time) {
 		//none
 	}
+
+	private double calculateAvgWaiting() {
+		double totalWaiting = 0;
+		for (int waitingTime: waitingTimes) {
+			totalWaiting += waitingTime;
+		}
+		return totalWaiting / waitingTimes.size();
+	}
+
 
 	@Override
 	public String getAlgorithmName() {
@@ -49,7 +79,7 @@ public class SJF implements Scheduler {
 		if (queue.isEmpty()) {
 			return null;
 		}
-		return queue.get(0);
+		return queue.getFirst();
 	}
 
 	@Override
